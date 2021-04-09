9e1749c75ea30825c330bcdb16b3ee8a0c049675
Rockwell Weiner <rockwellw@users.noreply.github.com>
2021-04-09 11:35:16 -0700

    Update fingerprint64.sql

--- functions/farmhash/sql/fingerprint64.sql
+++ functions/farmhash/sql/fingerprint64.sql
@@ -1,5 +1,5 @@
 create or replace function `GCP_PROJECT_ID`.farmhash.fingerprint64(str string)
 returns string
 as (
-  `GCP_PROJECT_ID`.farmhash.int64ToUint64String(farm_fingerprint(str))
+  CASE WHEN farm_fingerprint(str) < 0 THEN `GCP_PROJECT_ID`.farmhash.int64ToUint64String(farm_fingerprint(str)) ELSE farm_fingerprint(str) END
 );

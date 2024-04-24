42348cdf2395fb65b96354a06e8804506d93f402
Brett Naul <brettnaul@gmail.com>
2024-04-24 08:53:39 -0400

    Reimplement uint64<->int64 in BigQuery SQL

--- functions/farmhash/int64ToUint64String.js
+++
@@ -1,8 +0,0 @@
-function int64ToUint64String(num) {
-    var out = num;
-    if (num < 0) {
-        // 2^64 - > 18446744073709551616
-        out = BigInt(num) + 18446744073709551616n;
-    }
-    return out.toString();
-}
